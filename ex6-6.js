90fb171ecd2535ffe2ae738dfcb153d4076906fa
sogo-yoshi <zm25089@josai.ac.jp>
2025-11-11 09:28:18 +0900

    Refactor showMessage and update who66 variable

--- ex6-6.js
+++ ex6-6.js
@@ -1,4 +1,4 @@
-function sayhello66() {alert("hello！");}
-function showMessage(name) {alert(name + "が来た");}
-function taro66() {showMessage("太郎");}
-function hanako66() {showMessage("花子");}
+var who66 =  'woeld';
+function showMessage() {alert(who66 + 'が来た');}
+function taro66() {who66 = '太郎';}
+function hanako66() {who66 = '花子';}
