4b592abf0a5b879c84c09db19c7923bc2db35d49
sogo-yoshi <zm25089@josai.ac.jp>
2025-11-11 09:33:22 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,4 +1,4 @@
 var who66 =  'woeld';
-function showMessage() {alert(who66 + 'が来た');}
+function sayhello66() {alert(who66 + 'が来た');}
 function taro66() {who66 = '太郎';}
 function hanako66() {who66 = '花子';}
