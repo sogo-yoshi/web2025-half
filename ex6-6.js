895bb94bdfdec4bc9418508b68a4c2e3b339782a
sogo-yoshi <zm25089@josai.ac.jp>
2025-11-11 10:00:30 +0900

    Refactor sayhello66 and update greeting message

--- ex6-6.js
+++ ex6-6.js
@@ -1,3 +1,8 @@
-function sayhello66() {alert(who66 + 'が来た');}
+var who66 = 'world';
+function sayhello66() {alert('Hello, ' + who66 + '!');}
+
 function taro66() {who66 = '太郎';}
 function hanako66() {who66 = '花子';}
+
+function taro66() {alert(who66 + 'が来た');}
+function hanako66() {alert(who66 + 'が来た');}
