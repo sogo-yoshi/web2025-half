45b12fc86e295259b21d4f8ae35a33c5aa93f5d0
sogo-yoshi <zm25089@josai.ac.jp>
2025-11-11 10:13:58 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,8 +1,9 @@
 var who66 = 'world';
+
 function sayhello66() {alert('Hello, ' + who66 + '!');}
 
-function taro66() {who66 = '太郎';}
-function hanako66() {who66 = '花子';}
+function setTaro() {who66 = '太郎';}
 
-function taro66() {alert('太郎' + 'が来た');}
-function hanako66() {alert('花子' + 'が来た');}
+function setHanako() {who66 = '花子';}
+
+function showComing() {alert(who66 + 'が来た');
