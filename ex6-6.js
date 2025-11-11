ee2bd37b964e0a4f2b9730d3afb24a9692616d28
sogo-yoshi <zm25089@josai.ac.jp>
2025-11-11 10:18:47 +0900

    Refactor functions for handling names

--- ex6-6.js
+++ ex6-6.js
@@ -2,8 +2,6 @@ var who66 = 'world';
 
 function sayhello66() {alert('Hello, ' + who66 + '!');}
 
-function setTaro() {who66 = '太郎';}
+function taro66() {who66 = '太郎';alert('太郎が来た');}
 
-function setHanako() {who66 = '花子';}
-
-function showComing() {alert(who66 + 'が来た');
+function hanako66() {who66 = '花子';alert('花子が来た');}
