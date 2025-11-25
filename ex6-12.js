317b61d766765d6af84fe7dd33f2f476abd04a8f
sogo-yoshi <zm25089@josai.ac.jp>
2025-11-25 09:53:34 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,3 +1,9 @@
-function gazo610() {
-    document.getElementById('who610').innerHTML = '<img src="./image/banana_peel.png" alt="バナナの皮" width="200">';
+function gazo612() {
+    document.getElementById('who612').innerHTML =
+        '<img src="./image/banana_peel.png" alt="バナナの皮" width="200">';
+}
+
+function gazoReset612() {
+    document.getElementById('who612').innerHTML =
+        '<img src="./image/banana.png" alt="バナナ" width="200">';
 }
