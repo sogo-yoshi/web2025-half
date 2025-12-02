5b0819df58e5b93627e03e32cdb43d836555ffa5
sogo-yoshi <zm25089@josai.ac.jp>
2025-12-02 10:04:13 +0900

    Create ex6-13.js

---
+++ ex6-13.js
@@ -0,0 +1,9 @@
+function imgchange1() {
+  const img = document.getElementById('logo');
+  if (img) img.src = './image/banana_peel.png';
+}
+
+function imgReset1() {
+  const img = document.getElementById('logo');
+  if (img) img.src = './image/banana.png';
+}
